f7f6fc4e9669b9bed3e1cb6911aa346a682fc34a
Shifa <shifachugh06@gmail.com>
2026-02-03 19:40:05 +0530

    code in process

---
+++ RECURSION/RECURSIONpractice/EmailSystem.java
@@ -0,0 +1,10 @@
+package RECURSIONpractice;
+
+public class EmailSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(system.in);
+        int n = sc.nextLine();
+        System.out.println("Enetr character to remove: ");
+        char ch = sc.next.charAt(0);
+    }
+}
