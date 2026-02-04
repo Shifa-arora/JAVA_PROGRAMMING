5232fa04b104ba82425ad0ec1ef8f4c10873d6ad
Shifa <shifachugh06@gmail.com>
2026-02-04 08:50:57 +0530

    mistakely class push kr di abb java file k rhi hu

--- RECURSION/RECURSIONpractice/EmailSystem.java
+++ RECURSION/RECURSIONpractice/EmailSystem.java
@@ -1,10 +1,28 @@
 package RECURSIONpractice;
-
+import java.util.*;
 public class EmailSystem {
+    public static String removechar(String str, char ch){
+        if(str.length()==0){
+            return "";
+        }
+
+        String Rest = removechar(str.substring(1), ch);
+
+        if(str.charAt(0)==ch){
+            return Rest;
+        }
+        else{
+            return str.charAt(0) + Rest;
+        }
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(system.in);
-        int n = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
         System.out.println("Enetr character to remove: ");
-        char ch = sc.next.charAt(0);
+        char ch = sc.next().charAt(0);
+
+        String ans = removechar(n, ch);
+
+        System.out.println(ans);
     }
 }
